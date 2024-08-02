fd049ea317dedc223b512f22dab2026de397faf4
samayk27 <samaykumar2712@gmail.com>
2024-08-02 13:19:20 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,28 @@
+//Primitive Data Types
+
+//7 Types : String, Number, null, Boolean, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const secondId =Symbol('123')
+
+console.log(id===secondId) //returns false
+
+//Dynamically Typed language: Means that variables can hold data of any type till runtime, Data  type is not declared before compile time.
+
+//Reference Type (Non Primitive)
+
+//Objects, Arrays, Functions
+
+const heroes = ['Steve Rogers', 'Tony Stark','Bruce Banner','Scott Lang']
+
+let obj = {
+
+    myName: "Wade Wilson",
+    myAge: "Pretty Old",
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+//typeOf Null === Object
