0b557b16d41fa7632fd7b2259ab1c73575218dab
samayk27 <samaykumar2712@gmail.com>
2024-08-02 10:21:20 +0000

    datatypes

---
+++ 01_Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail ="something@hotmail.com"
+var accountPassword ="Hello"
+let accountState; //prints undefined in table
+/* 
+Don't use var
+Because of issue in block scope and functional scope
+*/
+
+//accountId =2; // not allowed
+
+accountEmail =" hc@hc.com"
+accountPassword =" bye"
+
+console.table([accountId, accountEmail, accountPassword,accountState]) //tabular structure
+
+console.log(accountId);
