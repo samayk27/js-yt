e3076feff4467377673b00884bcb53146399250f
samayk27 <samaykumar2712@gmail.com>
2024-08-02 12:56:47 +0000

    comparision of data types

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,7 @@
+console.log(null>0);
+console.log(null>=0); // true because >= and > work differently as compared to ==, comparision sign converts null to 0
+console.log(null==0)
+
+//avoid using these type of comparisions
+
+
