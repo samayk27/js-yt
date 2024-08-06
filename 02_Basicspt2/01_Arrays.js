600ec9f8a516462034eb3421228673064c27bed3
samayk27 <samaykumar2712@gmail.com>
2024-08-06 10:57:36 +0000

    Arrays done

---
+++ 02_Basicspt2/01_Arrays.js
@@ -0,0 +1,17 @@
+// arrays 
+
+const myarray = [0,1,2,3,4,5]
+
+console.log(myarray[1]);
+
+// myarray.push(6)
+// console.log(myarray);
+// myarray.pop()
+// console.log(myarray);
+
+// myarray.unshift(9)  => adds 9 to the start
+// console.log(myarray);
+// myarray.shift()
+// console.log(myarray);
+
+console.log(myarray.includes(5)) //prints true/false if value is present
